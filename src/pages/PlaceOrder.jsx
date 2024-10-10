8a36ea6445696c599c9940c9672232a7a7fac541
StephenAdedayo <alustephenadedayo@gmail.com>
2024-10-10 17:53:10 +0100

    added razorpay

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -33,6 +33,36 @@ const PlaceOrder = () => {
 
   }
 
+  const initPay = (order) => {
+       const options = {
+        key : import.meta.env.VITE_RAZORPAY_KEY_ID,
+        amount : order.amount,
+        currency : order.currency,
+        name : 'Order Payment',
+        description : 'Order Payment',
+        order_id : order.id,
+        receipt : order.receipt,
+        handler : async (response) => {
+            console.log(response);
+            try {
+              const {data} = await axios.post(backendUrl + '/api/order/verifyRazorpay', response, {headers : {token}})
+              if(data.success){
+                navigate('/')
+                setCartItems({})
+              }
+            } catch (error) {
+              console.log(error);
+              toast.error(error.message)
+              
+            }
+            
+        }
+       }
+
+       const rzp = new window.Razorpay(options)
+       rzp.open()
+  }
+
   const {navigate, backendUrl, token, cartItems, setCartItems, getCartAmount, delivery_fee, products} = useContext(ShopContext)
 
 
@@ -92,6 +122,16 @@ const PlaceOrder = () => {
            } else{
             toast.error(responseStripe.data.message)
            }
+          break;
+
+          case 'razorpay' : 
+          
+          const responseRazorpay = await axios.post(backendUrl + '/api/order/razorpay', orderData, {headers : {token}})
+          if(responseRazorpay.data.success){
+            initPay(responseRazorpay.data.order);
+            
+          }
+
           break;
 
           default: 
